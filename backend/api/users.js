538623c33d33d0abf3b83695d8ccf782508d2261
Zaïnab <zeinab0695@gmail.com>
2026-07-16 20:06:13 +0200

    users route

--- backend/api/users.js
+++ backend/api/users.js
@@ -2,3 +2,11 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
+const authenticateToken = require('../middleware/authMiddleware');
+
+router.get('/me', authenticateToken, userController.getMyProfile);
+router.put('/me', authenticateToken, userController.updateMyProfile);
+router.delete('/me', authenticateToken, userController.deleteMyProfile);
+router.get('/:id', userController.getUserById);
+
+module.exports = router;
