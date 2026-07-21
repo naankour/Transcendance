7c8ab6882766c7befa7bf52d1a12978a575b95a7
Naziha Aankour <nazihaaankour@iMac-de-Naziha-001.lan>
2026-07-21 22:06:10 +0200

    controllers reviews done

--- backend/prisma/prismaClient.js
+++ backend/prisma/prismaClient.js
@@ -1,5 +1,3 @@
-import { PrismaClient } from './generated/prisma'
-
+const { PrismaClient } = require('./generated/prisma')
 const prisma = new PrismaClient()
-
-export default prisma
+module.exports = prisma
